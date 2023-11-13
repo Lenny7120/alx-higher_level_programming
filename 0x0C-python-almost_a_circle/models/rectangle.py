a2324636a6065e5ac213faec30d94d0adb6528af
Lenny7120 <ocrank20@gmail.com>
2023-11-13 16:10:24 +0000

    Task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,9 +85,22 @@ class Rectangle(Base):
         return self.__height * self.__width
 
     def display(self):
+        """Print the rectangle with '#' characters, considering x and y offsets."""
+        for _ in range(self.y):
+            print(" ")  # Print empty lines for y offset
+            for _ in range(self.x):
+                print(" ")
         """Print the rectangle with '#' characters."""
         for _ in range(self.height):
             print("#" * self.width)
     def __str__(self):
         """Return a string representation of the rectangle."""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+    def update(self, *args):
+        """Update attributes with the provided arguments."""
+        if len(args) > 0:
+            self.id = args[0] if len(args) >= 1 else self.id
+            self.width = args[1] if len(args) >= 2 else self.width
+            self.height = args[2] if len(args) >= 3 else self.height
+            self.x = args[3] if len(args) >= 4 else self.x
+            self.y = args[4] if len(args) >= 5 else self.y
